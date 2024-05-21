de664b9c16de6faa9b578ba65851e6bfda87cd8e
Katie Lin <katielin019@gmail.com>
2024-05-21 12:48:01 -0400

    add new script

---
+++ scripts/split_longer_words.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+src='9+_letter_words.txt'
+
+cd by_length
+
+for i in {9..15}; do
+    touch ${i}_letter_words.txt
+done
+
+for i in {9..15}; do
+    cat $src | grep -E "^[A-Z]{${i}}$" > ${i}_letter_words.txt
+done
