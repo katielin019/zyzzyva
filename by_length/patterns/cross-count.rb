f7ef328a43a97fd7a01f05a2cc8122cdb272fdff
Katie Lin <katielin019@gmail.com>
2024-12-29 18:20:08 -0800

    fix cross-count

--- by_length/patterns/cross-count.rb
+++ by_length/patterns/cross-count.rb
@@ -1,9 +1,26 @@
+# COUNTS = {1: ['A', 'J', 'S'],
+          # 2: ['B', 'K', 'T'],
+          # 3: ['C', 'L', 'U'],
+          # 4: ['D', 'M', 'V'],
+          # 5: ['E', 'N', 'W'],
+          # 6: ['F', 'O', 'X'],
+          # 7: ['G', 'P', 'Y'],
+          # 8: ['H', 'Q', 'Z'],
+          # 9: ['I', 'R']}
+
 def main
   input = File.new('../4_letter_words.txt')
   file = File.readlines(input)
-
-  file.each do |x|
-    puts x if x.match?(/^[A-Z]Y[A-Z]{2}$/)
+  alpha = ("A".."Z").to_a
+  file.each do |word|
+    word.strip!
+    # if word.match?(/^[A-EJ-NS-W]Y[A-EJ-NS-W]{2}$/)
+    if word.match?(/^[A-Z]Y[A-Z]{2}$/)
+      vals = word.chars.map{ |c| alpha.index(c) % 9 + 1 }
+      # vals = word.chars.map{ |c| (alpha.index(c) + 1) % 9 }
+      puts word if vals.inject(:+) == 14
+      # puts vals.inject(:+)
+    end
   end
   # file.length
 end
